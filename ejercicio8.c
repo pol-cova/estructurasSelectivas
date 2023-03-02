325bf91b80d9f6d719ca416c07b2a51677871daf
pol-cova <83374367+pol-cova@users.noreply.github.com>
2023-03-02 08:58:40 -0600

    Add more exercises.

--- ejercicio8.c
+++ ejercicio8.c
@@ -17,19 +17,19 @@ int main(){
      * el usuario
     */
     float a,b,c, disc, raiz1, raiz2;
-    char userOption;
 
     // Definimos el idioma a trabajar
     setlocale(LC_CTYPE, "SPANISH");
     /*
-     * La forma general es ax^2 + bx + c, asi que solicitara los coeficientes
+     * La forma general es ax^2 + bx + c = 0, asi que solicitara los coeficientes
      */
-    printf("La forma general es ax^2 + bx + c");
-    printf("Ingrese el coeficiente de a: ");
+    printf("La forma general es ax^2 + bx + c\n");
+    printf("-------------------------------------\n");
+    printf("Ingrese el coeficiente de a: \n");
     scanf("%f", &a);
-    printf("Ingrese el coeficiente de b: ");
+    printf("Ingrese el coeficiente de b: \n");
     scanf("%f", &b);
-    printf("Ingrese el coeficiente de c: ");
+    printf("Ingrese el coeficiente de c: \n");
     scanf("%f", &c);
 
     disc = (b * b) - 4*(a*c);
@@ -52,17 +52,13 @@ int main(){
         printf(" En este caso las raíces son iguales, entonces el valor de la raíz es: %.2f ", raiz2);
 
     } else if (disc < 0){
-        printf("Tu ecuación to tiene solución real, la solución es compleja desearías ver la solución compleja: s/n");
-        scanf("%c", &userOption);
+        printf("Tu ecuación no tiene solución real, la solución es compleja desearías ver la solución compleja\n");
+        printf("-----------------\n");
 
-        if (userOption == 's'){
-            disc = sqrtf(disc);
-            raiz1 = (-b + I * disc) / 2*a;
-            raiz2 = (-b - I * disc) / 2*a;
+        disc = -1 * disc;
 
-            printf("La raíz compleja 1 de tu ecuación es: %.2f", raiz1);
-            printf("La raíz compleja 2 de tu ecuación es: %.2f", raiz2);
-        }
+        printf("La raíz compleja 1 de tu ecuación es: (-%.2f + i √ %.2f )/ 2 %.2f \n", b,disc,a);
+        printf("La raíz compleja 1 de tu ecuación es: (-%.2f - i √ %.2f )/ 2 %.2f \n", b,disc,a);
     }
 
     system("pause");
