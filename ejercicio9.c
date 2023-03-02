2ce28127412737d7f662c1c4367758bfc87f1356
pol-cova <83374367+pol-cova@users.noreply.github.com>
2023-03-02 09:22:39 -0600

    Update version.

---
+++ ejercicio9.c
@@ -0,0 +1,14 @@
+//
+// Created by Paul Contreras on 02/03/23.
+//
+/*
+ * Calcule el pago de un trabajador al día, según las horas trabajadas y el precio por hora para el trabajo
+ * que se realice. Las categorías y pago por horas correspondientes son:
+    Intendencia: pago por hora $35.
+    Asistente : pago por hora $50.
+    Administrativo : pago por hora $80.
+    Directivo : pago por hora $120.
+    Gerente : pago por hora $150.
+
+ */
+
